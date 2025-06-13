ad87fb22bba8e3d1cdf060ffa3a2d568a2357e9d
ankitjhagithub21 <ajha45075@gmail.com>
2025-06-13 16:01:45 +0530

    changes dashboard page

--- revealjs/src/pages/Dashboard.jsx
+++ revealjs/src/pages/Dashboard.jsx
@@ -1,35 +1,36 @@
-import { useState } from "react"
-import CreateQuizSlide from "../components/CreateQuizSlide"
-import CreateVideoSlide from "../components/CreateVideoSlide"
-import CreateImageComparisonSlide from "../components/CreateImageComparisonSlide"
+import { useState } from "react";
+import CreateQuizSlide from "../components/CreateQuizSlide";
+import CreateVideoSlide from "../components/CreateVideoSlide";
+import CreateImageComparisonSlide from "../components/CreateImageComparisonSlide";
 
 const Dashboard = () => {
-  const slideTypes = ["quiz","video","image"] 
-  const [type,setType] = useState(slideTypes[0])
+  const slideTypes = ["quiz", "video", "image"];
+  const [type, setType] = useState(slideTypes[0]);
+
   return (
     <div>
-     <div className="mt-20">
-       <h2 className="text-3xl text-center">Create Slide</h2>
-      <div className="flex items-center justify-center gap-5  p-5">
-        <button onClick={()=>setType('quiz')} className={` ${type==='quiz' ? 'bg-green-600':'bg-gray-800'} px-4 py-2 cursor-pointer text-white rounded-lg`}>Quiz Slide</button>
-        <button onClick={()=>setType('video')} className={` ${type==='video' ? 'bg-green-600':'bg-gray-800'} px-4 py-2 cursor-pointer text-white rounded-lg`}>Video Slide</button>
-
-        <button onClick={()=>setType('image')} className={` ${type==='image' ? 'bg-green-600':'bg-gray-800'} px-4 py-2 cursor-pointer text-white rounded-lg`}>Image comarision Slide</button>
+      <div className="mt-20">
+        <h2 className="text-3xl text-center">Create Slide</h2>
+        <div className="flex items-center justify-center gap-5 p-5">
+          <select
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            className="px-4 py-2 rounded-lg outline-none text-gray-800 border-2 border-gray-300 cursor-pointer"
+          >
+            <option value="quiz">Quiz Slide</option>
+            <option value="video">Video Slide</option>
+            <option value="image">Image Comparison Slide</option>
+          </select>
+        </div>
       </div>
-     </div>
+
       <section>
-       {
-         type === 'quiz' && <CreateQuizSlide/>
-       }
-         {
-         type === 'video' && <CreateVideoSlide/>
-       }
-         {
-         type === 'image' && <CreateImageComparisonSlide/>
-       }
+        {type === "quiz" && <CreateQuizSlide />}
+        {type === "video" && <CreateVideoSlide />}
+        {type === "image" && <CreateImageComparisonSlide />}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
