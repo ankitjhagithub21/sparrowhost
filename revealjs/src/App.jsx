fce147e2b6bed7e5a7f34e707a94850332a61d86
ankitjhagithub21 <ajha45075@gmail.com>
2025-06-13 10:42:13 +0530

    added routing

--- revealjs/src/App.jsx
+++ revealjs/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css"
+import {Routes,Route} from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Slider from "./pages/Slider"
 
 const App = () => {
   return (
-    <div className="text-3xl">Hello world</div>
+    <Routes>
+      <Route path="/" element={<Slider/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+    </Routes>
   )
 }
 
